d69dc6a934888136066b6082ee7c8dbaf8271fe1
Tweizh <ghnmqdtg@gmail.com>
2025-03-16 15:16:58 +0800

    feat: add testing for regex

--- UnitTestHW/Regular Expression Matching/test_solution.py
+++ UnitTestHW/Regular Expression Matching/test_solution.py
@@ -1,26 +1,28 @@
-import ...
-from ...
+import pytest
+from solution import Solution
+
 
 @pytest.fixture
-...
-    
+def solution():
+    return Solution()
+
+
 testcases = [
     # s, p, expected_res
     ["", "", True],
     ["aa", "a", False],
     ["aa", "a*", True],
     ["ab", ".*", True],
-    ["a", ".*.", True],
     ["aab", "c*a*b", True],
-    ["aaa", "ab*a*c*a", True]
+    ["aaa", "ab*a*c*a", True],
 ]
 
-@pytest.mark.parametrize([?], testcases)
-...
+
+@pytest.mark.parametrize("s, p, expected", testcases)
+def test_solution(solution, s, p, expected):
     assert solution.isMatch(s, p) == expected
 
-"""
+
 @pytest.mark.xfail
 def test_broken_solution(solution):
-    assert solution.isMatch() == 
-"""
+    assert solution.isMatch("a", ".*.") == True
